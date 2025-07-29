5d56089daa5c66992f46d48bf52f041fcd525e8d
sholker <30467978+sholker@users.noreply.github.com>
2025-07-29 21:09:29 +0300

    link home

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
