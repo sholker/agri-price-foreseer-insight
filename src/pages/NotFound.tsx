e9349a2a06adb2cea577722418f4532c85620ad1
sholker <30467978+sholker@users.noreply.github.com>
2025-08-03 00:02:12 +0000

    chnage 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,68 +1,32 @@
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import "@/components/ui/NotFound.css"
-
 const NotFound = () => {
-  const location = useLocation();
+  const navigate = useNavigate();
 
-  // Log the 404 error to the console (this logic remains)
-  useEffect(() => {
-    console.error(
-      "404 Error: User attempted to access non-existent route:",
-      location.pathname
-    );
-  }, [location.pathname]);
+  const handleGoBack = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault(); // Prevent the default anchor link behavior
+    navigate(-1); // Navigate to the previous page in history
+  };
 
   return (
-    <div>
-      <svg id="yetiSVG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 470">
-        {/*
-          The entire SVG code provided in your prompt goes here.
-          I've added classes like "yeti-arm-l", "yeti-arm-r", "yeti-eye"
-          to the relevant groups for CSS targeting.
-        */}
-        <g id="yeti">
-          {/* ... other yeti parts ... */}
-          <g id="eyeL" className="yeti-eye">
-            <circle cx="135.9" cy="105.3" r="3.5" fill="#265D85"/>
-            <circle cx="133.7" cy="103.5" r="1" fill="#FFF"/>
-          </g>
-          <g id="eyeR" className="yeti-eye">
-            <circle cx="163.2" cy="96.3" r="3.5" fill="#265D85"/>
-            <circle cx="160.9" cy="94.5" r="1" fill="#FFF"/>
-          </g>
-          <g id="mouth" className="yeti-mouth">
-            <path fill="#265D85" d="M149 115.7c-4.6 3.7-6.6 9.8-5 15.6.1.5.3 1.1.5 1.6.6 1.5 2.4 2.3 3.9 1.7l11.2-4.4 11.2-4.4c1.5-.6 2.3-2.4 1.7-3.9-.2-.5-.4-1-.7-1.5-2.8-5.2-8.4-8.3-14.1-7.9-3.7.2-5.9 1.1-8.7 3.2z"/>
-          </g>
-          <g id="armR" className="yeti-arm-r">
-            {/* ... arm R paths ... */}
-          </g>
-          <g id="armL" className="yeti-arm-l">
-            {/* ... arm L paths ... */}
-          </g>
-        </g>
-        {/* ... etc. ... */}
-      </svg>
-      <svg id="lightSVG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 470">
-        <defs>
-          <filter id="white-glow">
-            {/* ... filter definition ... */}
-          </filter>
-        </defs>
-        <g id="light">
-          {/* ... light paths ... */}
-        </g>
-      </svg>
-      <div className="content">
-        <h3>Hello?? Is somebody there?!?</h3>
-        <p>
-          We know it’s scary, but the page you’re trying to reach can’t be
-          found. Perhaps it was just a bad <span>link</span> dream?
-        </p>
-         <a href="/" className="home-link">
-          Return to Home
+    <div className="not-found-container">
+      <div className="mars"></div>
+      <img src="https://assets.codepen.io/1538474/404.svg" className="logo-404" alt="404 Error" />
+      <img src="https://assets.codepen.io/1538474/meteor.svg" className="meteor" alt="Meteor" />
+      <p className="title">Oh no!!</p>
+      <p className="subtitle">
+        You’re either misspelling the URL <br /> or requesting a page that's no
+        longer here.
+        <br />
+        Remeber - " Garbage in, Garbage out :P "
+      </p>
+      <div style={{ textAlign: 'center' }}>
+        <a className="btn-back" href="#" onClick={handleGoBack}>
+          Back to previous page
         </a>
       </div>
+      <img src="https://assets.codepen.io/1538474/astronaut.svg" className="astronaut" alt="Astronaut" />
+      <img src="https://assets.codepen.io/1538474/spaceship.svg" className="spaceship" alt="Spaceship" />
     </div>
   );
 };
